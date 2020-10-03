ab16344584e15ac50d4c6f35ed06834f2bcf677f
servetgulnaroglu <servet.gulnaroglu@gmail.com>
2020-10-03 19:28:43 +0300

    first commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        max = max + 1;
+    }
+
+    public void guessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void guessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNewScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 2)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
